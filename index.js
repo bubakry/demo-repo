3ee746bd4d5e8b078ebc444a6ca16c9d0a383b61
Adedayo Odugbesan <odugbesan@icloud.com>
2022-04-25 11:37:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,4 +3,11 @@ function myFunction() {
   document.getElementById("demo2").innerHTML = "How are you?";
 }
 
+// How to create variables:
+var x;
+let y;
 
+// How to use variables:
+x = 5;
+y = 6;
+let z = x + y;
