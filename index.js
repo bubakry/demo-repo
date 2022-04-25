6c9f3ee34125bba3ede4490545588a9c041080a4
Adedayo Odugbesan <odugbesan@icloud.com>
2022-04-25 11:36:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,12 @@ function myFunction() {
   document.getElementById("demo1").innerHTML = "Hello Dolly!";
   document.getElementById("demo2").innerHTML = "How are you?";
 }
+// How to create variables:
+var x;
+let y;
 
+// How to use variables:
+x = 5;
+y = 6;
+let z = x + y;
 
